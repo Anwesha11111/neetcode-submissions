ecd57078a6585fbdb9f695335dc2fd0b71888670
Anwesha Mohapatra <anweshamohapatra2005@gmail.com>
2026-09-21 20:23:05 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+   int minp=INT_MAX;
+   int maxp=0;
+   for(int p:prices){
+    minp=min(minp,p);
+    maxp=max(maxp,p-minp);
+   }
+
+        return maxp;
+    }
+};
